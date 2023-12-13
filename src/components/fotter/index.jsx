1d1f72960b74bdc12959854451da922feede15cc
Mwaqas786 <onidea789@gmail.com>
2023-12-13 18:07:49 +0500

    add antd grid

--- src/components/fotter/index.jsx
+++ src/components/fotter/index.jsx
@@ -3,7 +3,7 @@ import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, AppstoreOutlined, UsergroupAddOutlined, ContactsOutlined ,UserOutlined} from '@ant-design/icons';
 const { Footer } = Layout;
-
+import { Col, Row } from 'antd';
 const Index = () => {
   return (
     <div>
@@ -24,32 +24,40 @@ const Index = () => {
           </Menu.Item>
         </Menu>
       </Footer> */}
+     
        <Footer style={{ textAlign: 'center', backgroundColor:"black",color:"white",marginTop:"62vh" }}>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ marginRight: '20px' }}>
-          <img src="https://profitsols.com/wp-content/uploads/2023/07/LOGO.png" alt="Logo" style={{ height: '30px' }} />
-        </div>
-        <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
+     <Row>
+        <Col flex={12}><div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ marginRight: '20px' }}>
+            <img src="https://profitsols.com/wp-content/uploads/2023/07/LOGO.png" alt="Logo" style={{ height: '30px' }} />
+            </div>
+            <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </Col>
 
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']} style={{ marginTop: '20px',fontSize:"24px", }}>
-        <Menu.Item key="home" icon={<HomeOutlined style={{fontSize:"24px"}} />} >
-          <Link to="/">Home</Link>
-        </Menu.Item>
+        <Col flex={12}><Menu  mode="horizontal" defaultSelectedKeys={['home']} style={{ marginTop: '20px',fontSize:"24px", }}>
+            <Menu.Item key="home" icon={<HomeOutlined style={{fontSize:"24px"}} />} >
+            <Link to="/">Home</Link>
+            </Menu.Item>
 
-        <Menu.Item key="about" icon={<AppstoreOutlined style={{fontSize:"24px"}}/>} >
-          <Link to="/about">About</Link>
-        </Menu.Item>
+            <Menu.Item key="about" icon={<AppstoreOutlined style={{fontSize:"24px"}}/>} >
+            <Link to="/about">About</Link>
+            </Menu.Item>
 
-        <Menu.Item key="contact" icon={<ContactsOutlined style={{fontSize:"24px"}}/>}>
-          <Link to="/contect">contect</Link>
-        </Menu.Item>
-        <Menu.Item key="Sercious"  icon={<UsergroupAddOutlined style={{fontSize:"24px"}}/>} >
-          <Link to="/servious" >Sercious</Link>
-        </Menu.Item>
-      </Menu>
+            <Menu.Item key="contact" icon={<ContactsOutlined style={{fontSize:"24px"}}/>}>
+            <Link to="/contect">contect</Link>
+            </Menu.Item>
+            <Menu.Item key="Sercious"  icon={<UsergroupAddOutlined style={{fontSize:"24px"}}/>} >
+            <Link to="/servious" >Sercious</Link>
+            </Menu.Item>
+        </Menu>
+        </Col>
+    </Row>
+      
+
+      
 
       <p style={{ marginTop: '20px' }}>© 2023 Shadow Aoo Company. All rights reserved.</p>
     </Footer>
