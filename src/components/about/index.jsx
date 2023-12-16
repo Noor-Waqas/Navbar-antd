674de6ce39bb7094eaa4ae99868cfcd594eadbf2
Mwaqas786 <onidea789@gmail.com>
2023-12-16 13:59:53 +0500

    add product list

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import style from './style.module.scss';
+
 
 const index = () => {
   return (
-    <div style={{height:"60vh"}}>
-        <h1>About page</h1>
+    <div >
+
+      <div className={style.ifaramStyle}>
+       <iframe width="100%" height="400" src="https://vrnl.xyz/?params=8015,TP2020" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div className={style.ifaramStyle}>
+       <iframe width="100%" height="400" src="https://vrnl.xyz/?params=8002,TEST" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div className={style.ifaramStyle}>
+       <iframe width="100%" height="400" src="https://vrnl.xyz/?params=8011,LUCKY7" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div className={style.ifaramStyle}>
+       <iframe width="100%" height="400" src="https://vrnl.xyz/?params=8017,LUCKY7B" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+
     </div>
   )
 }
